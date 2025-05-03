842e6872fce852c81b1ca9bb581253e50940588a
Arafat Ali <aravatt22@gmail.com>
2025-05-03 11:28:20 +0300

    feat: Initial commit for the Band name geneerator project

---
+++ Beginner/Day01/task.py
@@ -0,0 +1,5 @@
+greating = "Welcome to band name generator"
+print(greating)
+city =input("what is the name of the city you grew up ")
+pet = input(" what is your pet's name ")
+print("Your band name could be: " + city + "'" + "s " + pet)
