08e9b4b904be79f3ad8575e9b1a1cbb3e0fdc39a
Arafat Ali <aravatt22@gmail.com>
2025-07-13 11:17:19 +0300

    Quiz programm with interacting CLI pop up questions

---
+++ Beginner/Day17/quiz_brain.py
@@ -0,0 +1,31 @@
+import question_model
+
+
+class QuizBrain:
+	def __init__(self,q_list):
+		self.question_number = 0
+		self.score = 0
+		self.question_list = q_list
+
+	def still_has_questions(self):
+		return self.question_number < len(self.question_list)
+
+	def next_question(self):
+		current_question = self.question_list[self.question_number]
+		self.question_number+=1
+		user_answer =input(f"Q.{self.question_number}: {current_question.text}True/False?")
+		self.check_answer(user_answer , current_question.answer)
+
+	def check_answer(self , user_answer , correct_answer):
+		go = True
+		while go:
+			if user_answer.lower() == correct_answer.lower():
+				self.score += 1
+				print(f"You got it write . your score is : {self.score} / {self.question_number}")
+			else:
+				print(f"sorry! that's wrong. the correct answer was {correct_answer}")
+			print(f"your totoal score is : {self.score} / {self.question_number}")
+			go = False
+			return
+
+
