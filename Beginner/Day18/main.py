5037ce114a48b138097f9e2b1b32438151057df1
Arafat Ali <aravatt22@gmail.com>
2025-07-15 11:47:25 +0300

    the hirst painting project

--- Beginner/Day18/main.py
+++ Beginner/Day18/main.py
@@ -21,18 +21,5 @@ for dot_count in range(1, number_of_dots + 1):
 		tim.setheading(180)
 		tim.forward(500)
 		tim.setheading(0)
-
-
-
-
-
-
-
-
-
-
-
-
-
 screen = turtle_module.Screen()
 screen.exitonclick()
