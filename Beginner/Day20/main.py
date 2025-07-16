e4daaa55b6c28291b1a190a14235bda2de9609f7
Arafat Ali <aravatt22@gmail.com>
2025-07-16 10:39:17 +0300

    Day20

---
+++ Beginner/Day20/main.py
@@ -0,0 +1,37 @@
+from turtle import Screen , Turtle
+import  time
+from snake import Snake
+
+screen = Screen()
+screen.setup(600 , 600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+
+is_game_on = True
+while is_game_on:
+	screen.update()
+	time.sleep(0.1)
+	snake.move()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
